339cadf28ee9ad6d5391963538c4ef66f86e440d
Sk.Amir Hamza <sk.amirhamza@gmail.com>
2021-10-17 02:45:01 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
-sudo apt update && sudo apt install -y apache2
-sudo apt update && sudo apt install -y php libapache2-mod-php php-mysql
+#sudo apt update && sudo apt install -y apache2
+#sudo apt update && sudo apt install -y php libapache2-mod-php php-mysql
 
-sudo apt update
-sudo apt install -y build-essential
-sudo apt-get install -y manpages-dev
-sudo apt install -y default-jre
-sudo apt install -y python2
+#sudo apt update
+#sudo apt install -y build-essential
+#sudo apt-get install -y manpages-dev
+#sudo apt install -y default-jre
+#sudo apt install -y python2
 
-cd ..
-cd ..
-cd var/www/html
+cd ../../var/www/html
 cd git clone https://github.com/coderoj-dev/JudgerController
 cd JudgerController
 bash run.sh
