7f3288d54c8f5dbd5fb3f04e54968c70dd8a72c3
Sk.Amir Hamza <sk.amirhamza@gmail.com>
2021-10-17 04:11:50 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,58 @@
-printf "Installing Start..\n"
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+BLUE='\033[1;35m'
+WHITE='\033[1;37m'
+
+msg(){
+	printf " ${WHITE}- Installing  ${YELLOW}$1"
+}
+
+ok(){
+	printf " ${GREEN} (\u2714 )\n"
+}
+
+printf "\n${GREEN}Starting Install ${RED}CoderOJ ${GREEN}Judger Server\n\n"
+
+msg "Update"
 sudo apt update > update.txt 2> error.txt
-printf "Installing apache2..\n"
+ok
+
+msg "Apache2"
 sudo apt install -y apache2 > apache2.txt 2> error.txt
-printf "Installing php..\n"
+ok
+
+msg "PHP"
 sudo apt install -y php libapache2-mod-php php-mysql > php.txt 2> error.txt
+ok
 
+printf "\n"
 
-printf "Installing C++..\n"
+msg "C, C++, C++11"
 sudo apt install -y build-essential > cpp.txt 2> error.txt
 sudo apt-get install -y manpages-dev > cpp1.txt 2> error.txt
-printf "Installing Java..\n"
+ok
+msg "Java"
 sudo apt install -y default-jre > java.txt 2> error.txt
-printf "Installing Python2..\n"
+ok
+msg "Python2"
 sudo apt install -y python2 > python2.txt 2> error.txt
-printf "Installing Python3..\n"
+ok
+msg "Python3"
 sudo apt install -y python3 > python3.txt 2> error.txt
+ok
 
+printf "\n"
+printf " ${WHITE}- CD          ${YELLOW}/var/www/html\n"
 cd ../../var/www/html
-printf "Clone Github..\n"
+printf " ${WHITE}- Cloning    ${YELLOW} https://github.com/coderoj-dev/JudgerController"
 git clone https://github.com/coderoj-dev/JudgerController > gitclone.txt 2> error.txt
+ok
+printf " ${WHITE}- CD          ${YELLOW}JudgerController\n"
 cd JudgerController
+printf " ${WHITE}- Permission  ${YELLOW}run.sh"
 bash run.sh > runsh.txt 2> error.txt
+ok
 
-printf "Successfully Complete..\n"
+printf "\n${GREEN}Install Success ${RED}CoderOJ ${GREEN}Judger Server\n\n"
 
