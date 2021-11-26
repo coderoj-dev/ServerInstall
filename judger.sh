22fd8ba434c14f581345622136ce4f37df2002a0
Sk.Amir Hamza <sk.amirhamza@gmail.com>
2021-11-26 07:08:51 +0600

    Update judger.sh

--- judger.sh
+++ judger.sh
@@ -1,3 +1,4 @@
 (docker stop $(docker ps -a -q --filter ancestor=amirhamza05/judger:latest --format="{{.ID}}"))
 docker pull amirhamza05/judger:latest
 docker run -p 80:80 -d amirhamza05/judger:latest
+docker run -p 81:80 -d amirhamza05/judger:latest
